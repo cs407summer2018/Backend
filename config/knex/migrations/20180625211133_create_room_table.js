d6d010cb89526b1f6462ff697c46abc52fba421d
Noah Ignacio Smith <noahignaciosmith@gmail.com>
2018-06-26 23:22:39 -0400

    Added user, building, room, machine, and session tables

---
+++ config/knex/migrations/20180625211133_create_room_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('room', function(table) {
+        table.increments('id').primary()
+        table.integer('building_id').unsigned().references('id').inTable('building')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+        table.string('room_floor').notNullable()
+        table.string('room_number').notNullable()
+        table.timestamps()
+
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('room')
+};
---
+++ config/knex/migrations/20180626083318_create_session_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('session', function(table) {
+        table.increments('id').primary()
+        table.integer('machine_id').unsigned().references('id').inTable('machine')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+        table.integer('user_id').unsigned().references('id').inTable('user')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+        table.integer('pid').notNullable()
+        table.dateTime('start_time').notNullable()
+        table.dateTime('end_time').notNullable()
+        table.timestamps()
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('session')
+};
